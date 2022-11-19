bcb24624e6cbee9a092d0c2a5bda0752d4c9d826
SyrdiiDima <dimasyrdij@icloud.com>
2022-11-20 00:28:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="aleannlab-testwork">
+    <BrowserRouter basename="alean">
       <App />
     </BrowserRouter>
   </React.StrictMode>
